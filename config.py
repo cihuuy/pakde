24dc76de5b1d9295c7ec93765a9140bfd3b07f30
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-13 20:02:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ STRATUM_HOST = "0.0.0.0"
 STRATUM_PORT = 8080
 
 # Coin address where money goes. If you mine direct to the exchange, you MUST specify payment_id together with wallet of exchange.
-WALLET = '466KoUjvbFE2SduDyiZQUb5QviKo6qnbyDGDB46C6UcTDi5XmVtSXuRYJDmgd6mhYPU92xJHsTQyrSjLbsxdzKQc3Z1PZQM'
+WALLET = '86P42DaNTvmBmMLM4oL5kL6tVQVo9FfsnJDTqj6VU76whVzjMdMbMa7PV3SHAQuNySan44ToXVFn3gwFmqeDb58t1xqNVAB'
 # Only if you mine direct to the exchange
 PAYMENT_ID = ''
 
@@ -16,16 +16,16 @@ WORKER_ID_FROM_IP = False
 # On DwarfPool you have option to monitor your workers via email.
 # If WORKER_ID is enabled, you can monitor every worker/rig separately.
 MONITORING = True
-MONITORING_EMAIL = 'mail@example.com'
+MONITORING_EMAIL = 'aganagun553@gmail.com'
 
 # Main pool
-POOL_HOST = 'xmr-us.dwarfpool.com'
-POOL_PORT = 8050
+POOL_HOST = 'pool.hashvault.pro'
+POOL_PORT = 80
 
 # Failover pool
-POOL_FAILOVER_ENABLE = False
-POOL_HOST_FAILOVER = 'xmr-eu.dwarfpool.com'
-POOL_PORT_FAILOVER = 8050
+POOL_FAILOVER_ENABLE = True
+POOL_HOST_FAILOVER = 'pool.hashvault.pro'
+POOL_PORT_FAILOVER = 443
 
 # ERROR, INFO, DEBUG
 LOGLEVEL = 'DEBUG'
